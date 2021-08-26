c43980761a6b7290a1d95682e3cb15d2fdbe7ec6
Peter_YSQH <72824407+AprilSweettooth@users.noreply.github.com>
2021-08-26 18:49:01 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var users = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users', function (req, res) {
+    res.send(users);
 })
 
 app.listen(port, () => {
